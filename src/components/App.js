78b21ba58a24824ac0c62fdb5af8e40d637f6c02
Pratik <pratik@pop-os.localdomain>
2020-10-22 17:50:48 +0530

    Changes 1

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,35 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 class App extends Component {
-    constructor(props) {
-		super(props);
-	};
+  constructor(props) {
+    super(props);
+    this.state = {
+      para: "",
+    };
+  }
 
-    render() {
-    	return(
-    		<div id="main">
-				{ /* Do not remove this main div!! */ }
-    		</div>
-    	);
-    }
+  generatePara = () => {
+    this.setState({
+      para: (
+        <p id="para">
+          Hello, I've learnt to use the full-stack evaluation tool. This makes
+          me so happy
+        </p>
+      ),
+    });
+  };
+
+  render() {
+    return (
+      <div id="main">
+        <button id="click" onClick={this.generatePara}>
+          Click
+        </button>
+        {this.state.para}
+      </div>
+    );
+  }
 }
 
-
 export default App;
-
